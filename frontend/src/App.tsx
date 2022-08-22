b1770871894aef48d19524d862cd7de4c72a098f
tagovertag <tagovertag@gmail.com>
2022-08-22 01:06:26 -0300

    outros componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,18 @@
-import NotificationButton from "./componets/NotificationButton"
+import Header from "./componets/Header"
+import SalesCard from "./componets/SalesCard"
 
 
 function App() {
   return (
     < >
-      <h1>Olá mundo!</h1>
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
-      
+      <Header/>
+    <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+          </div>
+        </section>
+    </main>
     </ >
   )
 }
